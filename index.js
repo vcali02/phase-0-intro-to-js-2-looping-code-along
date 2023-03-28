d52c53f72b97d14e13a016aa2978aba2582841e8
valeria caliguire <caliguireval@gmail.com>
2023-03-27 19:37:58 -0600

    Adds veryHardLab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+const names = ["Guadalupe", "Ollie" , "Aki"];
+const eventName = "surprize"
+
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let e = 0; e < names.length; e++) {
+        messages.push(`Thank you, ${names[e]}, for the wonderful ${eventName} gift!`);
+    }
+        return messages;
+}
+
+  console.log(writeCards(names, eventName));
+
+
+  function countDown(integer) {
+    let m = 10;
+    while (m >= 0) {
+      console.log(m--);
+    }
+    return integer;
+  }
+  countDown(10);
